c43d60595e18df45f2c7a246c1d0c361c15aeb0b
Logec <dannybui@live.se>
2022-10-02 11:21:41 +0200

    Enemy
    Small enemy changes

--- Assets/Script/FastEnemy.cs
+++ Assets/Script/FastEnemy.cs
@@ -7,8 +7,25 @@ public class FastEnemy : Enemy
     //speed = 2*reuglar enemy speed
     public override void Start()
     {
-        fastSpeed = speed;
+        fastSpeed = speed * 5f;
     }
-    public override void MoveTowardsDragon(){}
-    public override void MoveTowardsExit(){}
+
+    public override void MoveTowardsDragon()
+    {
+        // transform.position += (dragon.transform.position - transform.position).normalized * fastSpeed * Time.deltaTime; 
+        
+    }
+    public override void MoveTowardsExit()
+    {
+        // transform.position += new Vector2(Random.Range(0f, 5f), Random.Range(0f, 5f)).normalized * fastSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hoard"))
+        {
+            holdingScrap = true;
+            fastSpeed = speed;
+        }
+    }
 }
