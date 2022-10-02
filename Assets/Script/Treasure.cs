8d342638e384843d0f0ee982e4abde8367f1f9b6
Erik Högberg <erik@e.email>
2022-10-02 13:28:26 +0200

    FastEnemy and Treasure
    Added in Enemy so that treasure only spawns if enemy got treasure

---
+++ Assets/Script/Treasure.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : MonoBehaviour
+{
+    public float targetTime = 3.0f;
+    public GameObject FastEnemy;
+    bool enemySpawned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        targetTime -= Time.deltaTime;
+
+        if(targetTime <= 0 && !enemySpawned)
+            {
+                Spawn();
+                enemySpawned = true;
+            }
+    }
+    void Spawn()
+    {
+        GameObject newEnemy = Instantiate(FastEnemy, new Vector2(40.0f, Random.Range(-10f, 10f)), Quaternion.identity);
+        newEnemy.GetComponent<FastEnemy>().currentHp = 5;
+        newEnemy.GetComponent<FastEnemy>().hp = 5;
+    }
+}
+
